758ec572ea9d49540c530627e89979e7b72bf1d0
Tres Serio <treserio@hotmail.com>
2022-08-23 20:00:57 -0500

    3 init

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,17 +1,24 @@
+const sha1 = require('sha1');
 const Mongo = require('../utils/db');
 
 class UsersController {
   static async postNew(req, res) {
     console.log(req.body);
     const { email, password } = req.body;
-    // confirm email exists
+    // confirm email exists & password were sent in req
     if (!email) return res.status(400).send('{"error": "Missing email"}');
     if (!password) return res.status(400).send('{"error": "Missing password"}');
-
-    if (Mongo.users.find({name: email})) return res.status(400).send('{"error": "Already exist"}');
-    return res.send("donkey dic");
+    // check if the user's email exists
+    console.log('wtf', await Mongo.users.findOne({ email }));
+    if (await Mongo.users.findOne({ email })) return res.status(400).send('{"error": "Already exist"}');
+    // create new user with sha1 hashed pw
+    const newUser = await Mongo.users.insertOne({
+      email,
+      password: sha1(password),
+    });
+    console.log('new user', newUser);
+    return res.status(201).json({ id: newUser.insertedId, email });
   }
 }
 
-// db.users.insertOne()
 module.exports = UsersController;
