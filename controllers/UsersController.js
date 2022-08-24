ce373edafa4532736b3e1960d336e6d1844700c9
Tres Serio <treserio@hotmail.com>
2022-08-23 20:02:43 -0500

    3.1 rm debugging logs

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,20 +3,18 @@ const Mongo = require('../utils/db');
 
 class UsersController {
   static async postNew(req, res) {
-    console.log(req.body);
+    // pull values from req's json data
     const { email, password } = req.body;
     // confirm email exists & password were sent in req
     if (!email) return res.status(400).send('{"error": "Missing email"}');
     if (!password) return res.status(400).send('{"error": "Missing password"}');
     // check if the user's email exists
-    console.log('wtf', await Mongo.users.findOne({ email }));
     if (await Mongo.users.findOne({ email })) return res.status(400).send('{"error": "Already exist"}');
     // create new user with sha1 hashed pw
     const newUser = await Mongo.users.insertOne({
       email,
       password: sha1(password),
     });
-    console.log('new user', newUser);
     return res.status(201).json({ id: newUser.insertedId, email });
   }
 }
